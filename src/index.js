84e5a5daa754a7d2ca5f5190820ac9ac88ee7910
Maxime Laforet <maxime10081985@gmail.com>
2017-03-29 20:38:59 +0100

    Add styles and make some adaptations for new design

--- src/index.js
+++ src/index.js
@@ -100,6 +100,7 @@ function passDataToView(drivers){
   }
 
   drivers.forEach( (driver, i) => {
+    driver.checkUndefinedValues()
     setTimeout( () => {
       view = document.createElement('div')
       view.className = 'driver-row'
@@ -179,28 +180,28 @@ renderDrivers()
 // Views
 
 function DriverView(driver, avatar, flag) {
-  driver.checkUndefinedValues()
+
   const view = `
       <div class="driver-row__item driver-row__item--first">
-        <span class="driver-ranking">${driver.ranking}°</span>
-        <div class="driver-points-wrap"><span class="driver-points-label">Points</span><span class="driver-points">${driver.points}</span></div>
+        <span class="driver-ranking">${driver.ranking}</span>
+        <div class="driver-points-wrap"><span class="driver-points">${driver.points}</span><span class="driver-points-label">Points</span></div>
       </div>
       <div class="driver-row__item driver-row__item--second">
         <img class="driver-photo" src="images/drivers/${avatar}.png" alt="">
       </div>
       <div class="driver-row__item driver-row__item--third">
-        <img class="driver-flag" src="images/flags/${flag}.jpg" alt="">
+        <img class="driver-flag" src="images/flags/${flag}.svg" alt="">
       </div>
+      <span class="driver-nationality">${driver.nationality}</span>
       <div class="driver-details">
-        <span class="driver-nationality">${driver.nationality}</span>
         <div><span class="driver-surname">${driver.surname}</span> <span class="driver-name">${driver.name}</span></div>
         <div><span class="driver-team">${driver.team}</span> <span class="driver-team-points">(${driver.teamPoints})</span></div>
       </div>
       <ul class="driver-row__item driver-races-data">
-        <li class="driver-races-data__item"><span class="races-data-label">Last GP</span><span class="race-data-figure">${driver.lastRace}°</span></li>
-        <li class="driver-races-data__item"><span class="races-data-label">Victories</span><span class="race-data-figure">${driver.wins}</span></li>
-        <li class="driver-races-data__item"><span class="races-data-label">Poles</span><span class="race-data-figure">${driver.poles}</span></li>
-        <li class="driver-races-data__item"><span class="races-data-label">Best Position</span><span class="race-data-figure">${driver.bestPosition}°</span><span class="race-data-figure">${driver.bestPositionTimes}</span></li>
+        <li class="driver-races-data__item"><span class="race-data-figure">${driver.lastRace}°</span><span class="races-data-label">Last GP</span></li>
+        <li class="driver-races-data__item"><span class="race-data-figure">${driver.wins}</span><span class="races-data-label">Victories</span></li>
+        <li class="driver-races-data__item"><span class="race-data-figure">${driver.poles}</span><span class="races-data-label">Poles</span></li>
+        <li class="driver-races-data__item"><span class="race-data-figure">${driver.bestPosition}°</span><span class="race-data-figure race-data-figure--tiny">${driver.bestPositionTimes}</span><span class="races-data-label">Best Position</span></li>
       </ul>
   `
   return view
